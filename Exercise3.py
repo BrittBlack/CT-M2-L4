99c9c3fdc23e82152232a8d658e16ddf39587d78
BrittBlack <brittneysblackmon@gmail.com>
2024-05-07 21:12:11 -0500

    updated exercise

---
+++ Exercise3.py
@@ -0,0 +1,16 @@
+#Loop Condition Logic
+#Task 1
+
+goats = 0
+while True:
+    print("I love goats. They are awesome!")
+    goats += 1
+    if goats >= 5:
+        break
+        
+#Task 2
+goats = 0
+while goats >= 0:
+    goats += 1
+if goats >= 5:
+     print("I love goats. They are awesome!")
